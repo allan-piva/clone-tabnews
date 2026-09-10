4aac48856ffcabb18a1dae2e52841b261b85b4d1
Allan Piva <allan3p@gmail.com>
2026-09-10 14:26:33 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return  <h1>Hello lovers in the office. Love you so much!! Cocozis hehe!!</h1>
+  return <h1>Hello lovers in the office. Love you so much!! Cocozis hehe!!</h1>;
 }
 
 export default Home;
