b871c6d64800e20461774573230f3d4ad9490b17
Allan Piva <allan3p@gmail.com>
2026-09-14 21:22:37 +0000

    Página em construção

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1>Hello lovers in the office. Love you so much!! Cocozis hehe!!</h1>;
+  return (
+    <h1 class="jsx-57b6de775625ba1a text-gray-800 text-xl font-bold">
+      InvestData - Site em construção... Para quem busca não só dados do
+      mercado, mas para quem quer aprender e ensinar
+    </h1>
+  );
 }
 
 export default Home;
